f4170acdf4f6600d6cd09b1efbc201fbc43f5857
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2012-06-19 16:34:28 +0900

    Add Net::SMB::CCTX#on_auth

--- ext/net/smb/smb.c
+++ ext/net/smb/smb.c
@@ -1,8 +1,8 @@
 #include <ruby.h>
 #include <libsmbclient.h>
 
-struct smbcctx {
-  SMBCCTX       *ctx;
+struct rb_smbcctx_data {
+  SMBCCTX       *smbcctx;
   VALUE         auth_callback;
 };
 
@@ -12,81 +12,181 @@ struct smbcctx {
 #define BOOL2VALUE(b)   ((b) ? Qtrue : Qfalse)
 #define VALUE2BOOL(v)   ((b) ? Qtrue : Qfalse)
 
-#define SMBCCTX_DECLARE_FROM_SELF(self, cctx) \
-  struct smbcctx *cctx; \
-  Data_Get_Struct(self, struct smbcctx, cctx);
+#define RB_SMBCCTX_DATA_FROM_SELF(self, data) \
+  struct rb_smbcctx_data *data; \
+  Data_Get_Struct(self, struct rb_smbcctx_data, data);
 
-static VALUE eRuntimeError;
+static VALUE rb_smb_eError;
 
 /* ====================================================================== */
 
-static void smbcctx_gc_mark(struct smbcctx *smbcctx)
+static void smbcctx_gc_mark(struct rb_smbcctx_data *data)
 {
-  rb_gc_mark(smbcctx->auth_callback);
+  rb_gc_mark(data->auth_callback);
 }
 
-static void smbcctx_free(struct smbcctx *smbcctx)
+static void smbcctx_auth_fn(SMBCCTX *smbcctx,
+	const char *server, const char *share,
+	char *workgroup, int wgmaxlen,
+	char *username, int unmaxlen,
+	char *password, int pwmaxlen)
 {
-  smbc_free_context(smbcctx->ctx, 1);
+  VALUE self;
+  VALUE ary;
+  VALUE wg;
+  VALUE un;
+  VALUE pw;
+
+  self = (VALUE)smbc_getOptionUserData(smbcctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
+
+  if (NIL_P(data->auth_callback)) {
+    return;
+  }
+
+  ary = rb_funcall(data->auth_callback,
+    rb_intern("call"), 2,
+    rb_str_new2(server),
+    rb_str_new2(share));
+
+  if (TYPE(ary) != T_ARRAY) {
+    return;
+  }
+  if (RARRAY_LEN(ary) != 3) {
+    rb_raise(rb_eArgError,
+	"Array should contain workgroup, "
+	"username and password to use as authentication");
+  }
+  
+  wg = RARRAY_PTR(ary)[0];
+  un = RARRAY_PTR(ary)[1];
+  pw = RARRAY_PTR(ary)[2];
+
+  if (!NIL_P(wg)) {
+    SafeStringValue(wg);
+    if (RSTRING_LEN(wg) > wgmaxlen - 1) {
+      rb_raise(rb_eArgError, "Workgroup too long");
+    }
+    strcpy(workgroup, RSTRING_PTR(wg));
+  }
+  else {
+    strcpy(workgroup, "");
+  }
+  if (!NIL_P(un)) {
+    SafeStringValue(un);
+    if (RSTRING_LEN(un) > unmaxlen - 1) {
+      rb_raise(rb_eArgError, "Username too long");
+    }
+    strcpy(username, RSTRING_PTR(un));
+  }
+  else {
+    strcpy(username, "");
+  }
+  if (!NIL_P(pw)) {
+    SafeStringValue(pw);
+    if (RSTRING_LEN(pw) > pwmaxlen - 1) {
+      rb_raise(rb_eArgError, "Password too long");
+    }
+    strcpy(password, RSTRING_PTR(pw));
+  }
+  else {
+    strcpy(password, "");
+  }
+}
+
+static void smbcctx_free(struct rb_smbcctx_data *data)
+{
+  smbc_free_context(data->smbcctx, 1);
 }
 
 static VALUE rb_smbcctx_alloc(VALUE klass)
 {
-  struct smbcctx *smbcctx = ALLOC(struct smbcctx);
+  struct rb_smbcctx_data *data = ALLOC(struct rb_smbcctx_data);
 
-  memset(smbcctx, 0, sizeof(struct smbcctx));
+  memset(data, 0, sizeof(*data));
 
   /* FIXME: Unset $HOME to ignore $HOME/.smb/smb.conf */
-  smbcctx->ctx = smbc_new_context();
-  if (smbcctx->ctx == NULL) {
-    rb_sys_fail("Cannot create SMBCCTX");
+  data->smbcctx = smbc_new_context();
+  if (data->smbcctx == NULL) {
+    rb_sys_fail("smbc_new_context()");
   }
 
-  smbcctx->auth_callback = Qnil;
+  data->auth_callback = Qnil;
 
-  return Data_Wrap_Struct(klass, smbcctx_gc_mark, smbcctx_free, smbcctx);
+  return Data_Wrap_Struct(klass, smbcctx_gc_mark, smbcctx_free, data);
 }
 
 static VALUE rb_smbcctx_initialize(VALUE self)
 {
-  SMBCCTX_DECLARE_FROM_SELF(self, cctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
 
-  smbc_setDebug(cctx->ctx, 0);
-  smbc_setOptionDebugToStderr(cctx->ctx, (smbc_bool)1);
+  smbc_setDebug(data->smbcctx, 0);
+  smbc_setOptionUserData(data->smbcctx, (void *)self);
+  smbc_setOptionDebugToStderr(data->smbcctx, (smbc_bool)1);
+  smbc_setFunctionAuthDataWithContext(data->smbcctx, smbcctx_auth_fn);
+
+  if (smbc_init_context(data->smbcctx) == NULL) {
+    rb_sys_fail("smbc_init_context()");
+  }
 
   return self;
 }
 
 static VALUE rb_smbcctx_debug_get(VALUE self)
 {
-  SMBCCTX_DECLARE_FROM_SELF(self, cctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
 
-  return INT2NUM(smbc_getDebug(cctx->ctx));
+  return INT2NUM(smbc_getDebug(data->smbcctx));
 }
 
 static VALUE rb_smbcctx_debug_set(VALUE self, VALUE debug)
 {
-  SMBCCTX_DECLARE_FROM_SELF(self, cctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
 
-  smbc_setDebug(cctx->ctx, NUM2INT(debug));
+  smbc_setDebug(data->smbcctx, NUM2INT(debug));
 
   return debug;
 }
 
 static VALUE rb_smbcctx_use_kerberos_get(VALUE self)
 {
-  SMBCCTX_DECLARE_FROM_SELF(self, cctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
 
-  return smbc_getOptionUseKerberos(cctx->ctx) ? Qtrue : Qfalse;
+  return smbc_getOptionUseKerberos(data->smbcctx) ? Qtrue : Qfalse;
 }
 
 static VALUE rb_smbcctx_use_kerberos_set(VALUE self, VALUE flag)
 {
-  SMBCCTX_DECLARE_FROM_SELF(self, cctx);
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
 
-  smbc_setOptionUseKerberos(cctx->ctx, (smbc_bool)(TRUE_P(flag) ? 1 : 0));
+  smbc_setOptionUseKerberos(data->smbcctx, (smbc_bool)(TRUE_P(flag) ? 1 : 0));
 
   return flag;
+}
+
+static VALUE rb_smbcctx_on_auth(int argc, VALUE* argv, VALUE self)
+{
+  RB_SMBCCTX_DATA_FROM_SELF(self, data);
+
+  VALUE proc;
+  VALUE block;
+
+  if (argc == 0 && !rb_block_given_p()) {
+    rb_raise(rb_eArgError, "No block or proc given");
+  }
+  else if (argc > 0 && rb_block_given_p()) {
+    rb_raise(rb_eArgError, "Cannot use both block and proc");
+  }
+
+  rb_scan_args(argc, argv, "01&", &proc, &block);
+  if (argc == 1) {
+    data->auth_callback = proc;
+  }
+  else {
+    data->auth_callback = block;
+  }
+
+  return Qnil;
 }
 
 /* ====================================================================== */
@@ -94,16 +194,23 @@ static VALUE rb_smbcctx_use_kerberos_set(VALUE self, VALUE flag)
 void Init_smb(void)
 {
   VALUE rb_mNet = rb_define_module("Net");
+  /* Net::SMB */
   VALUE rb_cSMB = rb_define_class_under(rb_mNet, "SMB", rb_cObject);
+  /* Net::SMB::CCTX */
   VALUE rb_cSMBCCTX = rb_define_class_under(rb_cSMB, "CCTX", rb_cObject);
 
+  /* Net::SMB::Error */
+  rb_smb_eError = rb_define_class_under(rb_cSMB, "Error", rb_eStandardError);
+
+  /* Net::SMB::CCTX */
   rb_define_alloc_func(rb_cSMBCCTX, rb_smbcctx_alloc);
   rb_define_method(rb_cSMBCCTX, "initialize", rb_smbcctx_initialize, 0);
   rb_define_method(rb_cSMBCCTX, "debug", rb_smbcctx_debug_get, 0);
   rb_define_method(rb_cSMBCCTX, "debug=", rb_smbcctx_debug_set, 1);
   rb_define_method(rb_cSMBCCTX, "use_kerberos", rb_smbcctx_use_kerberos_get, 0);
   rb_define_method(rb_cSMBCCTX, "use_kerberos=", rb_smbcctx_use_kerberos_set, 1);
+  rb_define_method(rb_cSMBCCTX, "on_auth", rb_smbcctx_on_auth, -1);
+  rb_define_alias(rb_cSMBCCTX, "on_authentication", "on_auth");
 
-  //eRuntimeError = rb_define_class_under(mSMB, "Net::SMB::RuntimeError", rb_eRuntimeError);
 }
 
