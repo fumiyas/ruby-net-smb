19431735f94da162953fa7c8109e9bcc25e59111
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2012-08-20 16:21:45 +0900

    test: Do setup only once

--- test/test_net_smb.rb
+++ test/test_net_smb.rb
@@ -8,106 +8,107 @@ require 'etc'
 module Net
 
 class SMBTest < Test::Unit::TestCase
+  @@test_dir = ENV['TEST_DIR'] = "#{Dir.getwd}/test"
+  @@username = Etc.getpwuid(Process.uid)['name']
+  @@password = 'password'
+
+  @@smb_conf = ENV['TEST_SMB_CONF'] ||= @@test_dir + "/etc/smb.conf"
+  @@smbd = ENV['TEST_SMBD'] ||= "smbd"
+  @@pdbedit = ENV['TEST_PDBEDIT'] ||= "pdbedit"
+  @@smbstatus = ENV['TEST_SMBSTATUS'] ||= "smbstatus"
+  @@samba_debug_level = ENV['TEST_SAMBA_DEBUGLEVEL'] ||= "10"
+  @@samba_log_dir = ENV['TEST_SAMBA_LOG_DIR'] ||= @@test_dir + "/log"
+  @@samba_var_dir = ENV['TEST_SAMBA_VAR_DIR'] ||= @@samba_log_dir + "/var"
+  @@share_dir = ENV['TEST_SHARE_DIR'] ||= @@samba_log_dir + "/share"
+
+  @@share_private =	"smb://localhost/private"
+  @@share_public =	"smb://localhost/public"
+  @@dir_noexist =	"dir.noexist"
+  @@dir_writeable =	"dir.writeable"
+  @@dir_writeable_m =	"ディレクトリ.writeable"
+  @@dirs_writeable =	[@@dir_writeable, @@dir_writeable_m]
+  @@dir_readable =	"dir.readable"
+  @@dirs_readable =	[@@dir_readable]
+  @@dir_noaccess =	"dir.noaccess"
+  @@dirs_noaccess =	[@@dir_noaccess]
+  @@dirs = [".", ".."] + @@dirs_readable + @@dirs_writeable + @@dirs_noaccess
+  @@file_noexist =	"file.noexist"
+  @@file_writeable =	"file.writeable"
+  @@file_writeable_m =	"ファイル.writeable"
+  @@files_writeable =	[@@file_writeable, @@file_writeable_m]
+  @@file_readable =	"file.readable"
+  @@files_readable =	[@@file_readable]
+  @@file_noaccess =	"file.noaccess"
+  @@files_noaccess =	[@@file_noaccess]
+  @@file_large =	"file.large"
+  @@files_misc =	[@@file_large]
+  @@files = @@files_readable + @@files_writeable + @@files_noaccess + @@files_misc
+
+  ENV['SMB_CONF_PATH'] = nil;
+  ENV['LIBSMB_PROG'] ||= @@test_dir + "/bin/smbd.wrapper"
+
+  ## Rotate log directory
+  if File.exist?(@@samba_log_dir + '.9')
+    system('/bin/rm', '-rf', @@samba_log_dir + '.9');
+  end
+  if File.exist?(@@samba_log_dir)
+    File.rename(@@samba_log_dir, @@samba_log_dir + '.0')
+  end
+  9.downto(1) do |i|
+    logdir_a = @@samba_log_dir + '.' + (i-1).to_s
+    logdir_b = @@samba_log_dir + '.' + i.to_s
+    if File.exist?(logdir_a)
+      File.rename(logdir_a, logdir_b)
+    end
+  end
+
+  Dir.mkdir(@@samba_log_dir, 0750)
+  Dir.mkdir(@@samba_var_dir, 0750)
+  Dir.mkdir(@@share_dir, 0750)
+  @@dirs_readable.each do |dname|
+    Dir.mkdir(@@share_dir + '/' + dname, 0550)
+  end
+  @@dirs_writeable.each do |dname|
+    Dir.mkdir(@@share_dir + '/' + dname, 0750)
+  end
+  @@dirs_noaccess.each do |dname|
+    Dir.mkdir(@@share_dir + '/' + dname, 0000)
+  end
+  @@files_readable.each do |fname|
+    File.open(@@share_dir + '/' + fname, "wb", 0440) do |file|
+      file.write(fname)
+    end
+  end
+  @@files_writeable.each do |fname|
+    File.open(@@share_dir + '/' + fname, "wb", 0660) do |file|
+      file.write(fname)
+    end
+  end
+  @@files_noaccess.each do |fname|
+    File.open(@@share_dir + '/' + fname, "wb", 0000) do |file|
+      file.write(fname)
+    end
+  end
+
+  File.open(@@share_dir + '/' + @@file_large, "wb", 0660) do |file|
+    random_chars = (0...100).map { rand(256).chr }.join("")
+    100000.times do |n|
+      file.write(random_chars)
+    end
+  end
+
+  pdbedit_r, pdbedit_w = IO.pipe
+  pdbedit_pid = Kernel.spawn(
+    @@pdbedit, "--configfile", @@smb_conf, "--create", "--password-from-stdin", @@username,
+    :in => pdbedit_r,
+    [:out, :err] => [@@samba_log_dir + '/pdbedit.log', 'w'],
+  )
+  pdbedit_r.close
+  pdbedit_w.print(@@password, "\n")
+  pdbedit_w.print(@@password, "\n")
+  pdbedit_w.close
+
   def setup
-    @test_dir = ENV['TEST_DIR'] = "#{Dir.getwd}/test"
-    @username = Etc.getpwuid(Process.uid)['name']
-    @password = 'password'
-
-    @smb_conf = ENV['TEST_SMB_CONF'] ||= @test_dir + "/etc/smb.conf"
-    @smbd = ENV['TEST_SMBD'] ||= "smbd"
-    @pdbedit = ENV['TEST_PDBEDIT'] ||= "pdbedit"
-    @smbstatus = ENV['TEST_SMBSTATUS'] ||= "smbstatus"
-    @samba_debug_level = ENV['TEST_SAMBA_DEBUGLEVEL'] ||= "10"
-    @samba_log_dir = ENV['TEST_SAMBA_LOG_DIR'] ||= @test_dir + "/log"
-    @samba_var_dir = ENV['TEST_SAMBA_VAR_DIR'] ||= @samba_log_dir + "/var"
-    @share_dir = ENV['TEST_SHARE_DIR'] ||= @samba_log_dir + "/share"
-
-    @share_private =	"smb://localhost/private"
-    @share_public =	"smb://localhost/public"
-    @dir_noexist =	"dir.noexist"
-    @dir_writeable =	"dir.writeable"
-    @dir_writeable_m =	"ディレクトリ.writeable"
-    @dirs_writeable =	[@dir_writeable, @dir_writeable_m]
-    @dir_readable =	"dir.readable"
-    @dirs_readable =	[@dir_readable]
-    @dir_noaccess =	"dir.noaccess"
-    @dirs_noaccess =	[@dir_noaccess]
-    @dirs = [".", ".."] + @dirs_readable + @dirs_writeable + @dirs_noaccess
-    @file_noexist =	"file.noexist"
-    @file_writeable =	"file.writeable"
-    @file_writeable_m =	"ファイル.writeable"
-    @files_writeable =	[@file_writeable, @file_writeable_m]
-    @file_readable =	"file.readable"
-    @files_readable =	[@file_readable]
-    @file_noaccess =	"file.noaccess"
-    @files_noaccess =	[@file_noaccess]
-    @file_large =	"file.large"
-    @files_misc =	[@file_large]
-    @files = @files_readable + @files_writeable + @files_noaccess + @files_misc
-
-    ENV['SMB_CONF_PATH'] = nil;
-    ENV['LIBSMB_PROG'] ||= @test_dir + "/bin/smbd.wrapper"
-
-    ## Rotate log directory
-    if File.exist?(@samba_log_dir + '.9')
-      system('/bin/rm', '-rf', @samba_log_dir + '.9');
-    end
-    if File.exist?(@samba_log_dir)
-      File.rename(@samba_log_dir, @samba_log_dir + '.0')
-    end
-    9.downto(1) do |i|
-      logdir_a = @samba_log_dir + '.' + (i-1).to_s
-      logdir_b = @samba_log_dir + '.' + i.to_s
-      if File.exist?(logdir_a)
-	File.rename(logdir_a, logdir_b)
-      end
-    end
-
-    Dir.mkdir(@samba_log_dir, 0750)
-    Dir.mkdir(@samba_var_dir, 0750)
-    Dir.mkdir(@share_dir, 0750)
-    @dirs_readable.each do |dname|
-      Dir.mkdir(@share_dir + '/' + dname, 0550)
-    end
-    @dirs_writeable.each do |dname|
-      Dir.mkdir(@share_dir + '/' + dname, 0750)
-    end
-    @dirs_noaccess.each do |dname|
-      Dir.mkdir(@share_dir + '/' + dname, 0000)
-    end
-    @files_readable.each do |fname|
-      File.open(@share_dir + '/' + fname, "wb", 0440) do |file|
-	file.write(fname)
-      end
-    end
-    @files_writeable.each do |fname|
-      File.open(@share_dir + '/' + fname, "wb", 0660) do |file|
-	file.write(fname)
-      end
-    end
-    @files_noaccess.each do |fname|
-      File.open(@share_dir + '/' + fname, "wb", 0000) do |file|
-	file.write(fname)
-      end
-    end
-
-    File.open(@share_dir + '/' + @file_large, "wb", 0660) do |file|
-      random_chars = (0...100).map { rand(256).chr }.join("")
-      100000.times do |n|
-	file.write(random_chars)
-      end
-    end
-
-    pdbedit_r, pdbedit_w = IO.pipe
-    pdbedit_pid = Kernel.spawn(
-      @pdbedit, "--configfile", @smb_conf, "--create", "--password-from-stdin", @username,
-      :in => pdbedit_r,
-      [:out, :err] => [@samba_log_dir + '/pdbedit.log', 'w'],
-    )
-    pdbedit_r.close
-    pdbedit_w.print(@password, "\n")
-    pdbedit_w.print(@password, "\n")
-    pdbedit_w.close
   end
 
   def teardown
@@ -116,9 +117,9 @@ class SMBTest < Test::Unit::TestCase
   def smbstatus
     smbstatus_r, smbstatus_w = IO.pipe
     smbstatus_pid = Kernel.spawn(
-      @smbstatus, "--configfile", @smb_conf, "--shares",
+      @@smbstatus, "--configfile", @@smb_conf, "--shares",
       :out => smbstatus_w,
-      :err => [@samba_log_dir + '/smbstatus.log', 'w+'],
+      :err => [@@samba_log_dir + '/smbstatus.log', 'w+'],
     )
     smbstatus_w.close
     smbstatus_r.readline
@@ -131,7 +132,7 @@ class SMBTest < Test::Unit::TestCase
   def smb
     smb = Net::SMB.new
     smb.auth_callback {|server, share|
-      [@username, @password]
+      [@@username, @@password]
     }
 
     return smb
@@ -140,27 +141,27 @@ class SMBTest < Test::Unit::TestCase
   def test_auth
     smb = Net::SMB.new
     smb.auth_callback {|server, share|
-      [@username, @password]
+      [@@username, @@password]
     }
     assert_nothing_raised do
-      smbdir = smb.opendir(@share_private)
+      smbdir = smb.opendir(@@share_private)
       smbdir.close
     end
 
     smb = Net::SMB.new
     smb.auth_callback {|server, share|
-      [@username, 'invalid-password']
+      [@@username, 'invalid-password']
     }
     assert_raise(Errno::EPERM) do
-      smb.opendir(@share_private)
+      smb.opendir(@@share_private)
     end
 
     smb = Net::SMB.new
     smb.auth_callback {|server, share|
-      ['invalid-user', @password]
+      ['invalid-user', @@password]
     }
     assert_raise(Errno::EACCES) do
-      smb.opendir(@share_private)
+      smb.opendir(@@share_private)
     end
 
     smb = Net::SMB.new
@@ -168,24 +169,24 @@ class SMBTest < Test::Unit::TestCase
       'blah-blah'
     }
     assert_raise(TypeError) do
-      smb.opendir(@share_private)
+      smb.opendir(@@share_private)
     end
 
     smb = Net::SMB.new
     smb.auth_callback {|server, share|
-      [@username]
+      [@@username]
     }
     assert_raise(ArgumentError) do
-      smb.opendir(@share_private)
+      smb.opendir(@@share_private)
     end
   end
 
   def test_dir_open_close
     smb = self.smb
 
-    smbdir = smb.opendir(@share_public)
+    smbdir = smb.opendir(@@share_public)
     assert_equal(smb.object_id, smbdir.smb.object_id)
-    assert_equal(@share_public, smbdir.url)
+    assert_equal(@@share_public, smbdir.url)
 
     assert_equal(false, smbdir.closed?)
     smbdir.close
@@ -205,37 +206,37 @@ class SMBTest < Test::Unit::TestCase
     end
 
     assert_raise(Errno::ENOENT) do
-      smbdir = smb.opendir(@share_public + '/' + @dir_noexist)
+      smbdir = smb.opendir(@@share_public + '/' + @@dir_noexist)
     end
     assert_raise(Errno::EACCES) do
-      smbdir = smb.opendir(@share_public + '/' + @dir_noaccess)
+      smbdir = smb.opendir(@@share_public + '/' + @@dir_noaccess)
     end
     ## Errno::ENOENT is not expected, but Samba 3.5 and 3.6 has a bug:
     ## https://bugzilla.samba.org/show_bug.cgi?id=9021
     assert_raise(Errno::ENOTDIR, Errno::ENOENT) do
-      smbdir = smb.opendir(@share_public + '/' + @file_writeable)
+      smbdir = smb.opendir(@@share_public + '/' + @@file_writeable)
     end
   end ## test_dir_open_close
 
   def test_dir_read
     smb = self.smb
-    dent_names_all = [*@dirs, *@files]
+    dent_names_all = [*@@dirs, *@@files]
 
-    smbdir = smb.opendir(@share_private)
+    smbdir = smb.opendir(@@share_private)
     dent_names = dent_names_all.clone
     while dent = smbdir.read
       assert_equal(dent.name, dent_names.delete(dent.name),
 	  "Unexpected directory entry: #{dent.name}")
-      if @dirs.include?(dent.name)
+      if @@dirs.include?(dent.name)
 	assert_equal(true, dent.dir?)
-      elsif @files.include?(dent.name)
+      elsif @@files.include?(dent.name)
 	assert_equal(true, dent.file?)
       end
     end
     assert_empty(dent_names)
     smbdir.close
 
-    smb.opendir(@share_public) do |smbdir|
+    smb.opendir(@@share_public) do |smbdir|
       dent_names = dent_names_all.clone
       while dent = smbdir.read
 	assert_equal(dent.name, dent_names.delete(dent.name),
@@ -247,9 +248,9 @@ class SMBTest < Test::Unit::TestCase
 
   def test_dir_seek
     smb = self.smb
-    dent_names_all = [*@dirs, *@files]
+    dent_names_all = [*@@dirs, *@@files]
 
-    smbdir = smb.opendir(@share_private)
+    smbdir = smb.opendir(@@share_private)
 
     smbdir_pos_all = Array.new
     fname_by_pos = Hash.new
@@ -298,9 +299,9 @@ class SMBTest < Test::Unit::TestCase
 
   def test_dir_enum
     smb = self.smb
-    dent_names_all = [*@dirs, *@files]
+    dent_names_all = [*@@dirs, *@@files]
 
-    smbdir = smb.opendir(@share_private)
+    smbdir = smb.opendir(@@share_private)
     dent_names = dent_names_all.clone
     smbdir.each do |dent|
       assert_equal(dent.name, dent_names.delete(dent.name))
@@ -308,7 +309,7 @@ class SMBTest < Test::Unit::TestCase
     assert_empty(dent_names)
     smbdir.close
 
-    smbdir = smb.opendir(@share_private)
+    smbdir = smb.opendir(@@share_private)
     dent_names = dent_names_all.clone
     smbdir.read
     smbdir.read
@@ -318,7 +319,7 @@ class SMBTest < Test::Unit::TestCase
     assert_empty(dent_names)
     smbdir.close
 
-    smbdir = smb.opendir(@share_private)
+    smbdir = smb.opendir(@@share_private)
     dent_names = dent_names_all.clone
     smbdir_enum = smbdir.each
     dent_names.size.times do |n|
@@ -336,8 +337,8 @@ class SMBTest < Test::Unit::TestCase
   def test_file_open_read_close
     smb = self.smb
 
-    @files_readable.each do |filename|
-      url = @share_public + '/' + filename
+    @@files_readable.each do |filename|
+      url = @@share_public + '/' + filename
       smbfile = smb.open(url)
 
       assert_equal(url, smbfile.url)
@@ -346,7 +347,7 @@ class SMBTest < Test::Unit::TestCase
 	smbfile.read(-1)
       end
 
-      assert_equal(@file_readable, smbfile.read)
+      assert_equal(@@file_readable, smbfile.read)
 
       assert_equal(false, smbfile.closed?)
       smbfile.close
@@ -373,8 +374,8 @@ class SMBTest < Test::Unit::TestCase
   def test_file_read_sequential
     smb = self.smb
 
-    file = File.open(@share_dir + '/' + @file_large)
-    smbfile = smb.open(@share_public + '/' + @file_large)
+    file = File.open(@@share_dir + '/' + @@file_large)
+    smbfile = smb.open(@@share_public + '/' + @@file_large)
 
     buffer_size = smbfile.read_buffer_size
     [
@@ -401,7 +402,7 @@ class SMBTest < Test::Unit::TestCase
   def test_file_read_eof
     smb = self.smb
 
-    smbfile = smb.open(@share_public + '/' + @file_readable)
+    smbfile = smb.open(@@share_public + '/' + @@file_readable)
 
     assert_equal(false, smbfile.eof?)
     smbfile.read
